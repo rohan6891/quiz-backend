6907b2ba146d28266d8b3d5b8a777e779770d51d
rohan6891 <rohan5144320@gmail.com>
2025-08-17 12:03:27 +0530

    fake commit

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const helmet = require('helmet');
 const rateLimit = require('express-rate-limit');
-require('dotenv').config();
 
 // Import routes
 const questionRoutes = require('./routes/questionRoutes');
